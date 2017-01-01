41fff2a926fe2a3b4b5b93ee70619491a2a938c1
Roee FL <roeefl@gmail.com>
2017-01-01 22:39:59 +0200

    Initial Commit - just the basics

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <GithubCorner></GithubCorner>
+  </div>
+</template>
+
+<script>
+  import GithubCorner from './GithubCorner'
+
+  export default {
+    name: 'app',
+    components: {
+      GithubCorner
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
